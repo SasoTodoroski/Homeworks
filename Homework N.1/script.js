7506d5cdfaa982f83688146131c5a014fab0d466
SasoTodoroski <sasopurde@yahoo.com>
2024-02-20 11:22:33 +0100

    gotova

--- Homework N.1/script.js
+++ Homework N.1/script.js
@@ -6,20 +6,36 @@ let inputBtn = document.getElementById("inputBtn");
 let resetBtn = document.getElementById("resetBtn");
 let tabela = document.getElementById("tabela");
 const loading = document.getElementById('loading');
+let countriesTalkingEnglishResult = document.getElementById('countriesTalkingEnglishResult');
 
 countiesTalkingEnglish.addEventListener('click', () => {
+    loading.style.display = 'block';
+    tabela.innerHTML = "";
+    callApiENG();
+});
+
+countiesUsingEUR.addEventListener('click', () => {
     loading.style.display = 'block';
     tabela.innerHTML = "";
     callApiEUR();
 });
 
+Macedonia.addEventListener('click', () => {
+    loading.style.display = 'block';
+    countriesTalkingEnglishResult.innerText = "";
+    tabela.innerHTML = "";
+    callApiMacedonia();
+});
+
 inputBtn.addEventListener('click', () => {
     loading.style.display = 'block';
+    countriesTalkingEnglishResult.innerText = "";
     tabela.innerHTML = "";
     callApi(countryName.value);
 });
 
 resetBtn.addEventListener('click', () => {
+    countriesTalkingEnglishResult.innerText = "";
     countryName.value = "";
     tabela.innerHTML = "";
 });
@@ -72,6 +88,36 @@ function callApi(name) {
         });
 }
 
+function callApiENG() {
+    fetch(`https://restcountries.com/v3.1/all`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            let languageData = [];
+            for (let i = 0; i < data.length; i++) {
+                const daliImaKeyLanguages = Object.keys(data[i]);
+                // console.log(daliImaKeyLanguages);
+                if (daliImaKeyLanguages.includes("languages"))
+                {    
+                    if (data[i].languages.eng)
+                    {   languageData.push(data[i].name.common);   }
+                }
+            }
+            loading.style.display = 'none';
+            // console.log("Counties talking English are:" + languageData);
+            countriesTalkingEnglishResult.innerText = `Countries talking english are:
+            
+            `
+            countriesTalkingEnglishResult.innerText += languageData;
+        })
+        .catch(function (err) {
+            console.error('Fetch error:', err);
+        });
+}
+
+// za zemji koi koristat euro
 function callApiEUR() {
     fetch(`https://restcountries.com/v3.1/all`)
         .then(function (response) {
@@ -81,40 +127,57 @@ function callApiEUR() {
             console.log(data);
             let languageData = [];
             for (let i = 0; i < data.length; i++) {
-                if (data[i].languages.eng) {
-                    languageData.push(data[i].name.common);
-                    console.log(data[i].languages);
-                    console.log(i);
-                    console.log(languageData);
+                const daliImaKeyCurrencies = Object.keys(data[i]);
+                console.log(daliImaKeyCurrencies);
+                if (daliImaKeyCurrencies.includes("currencies"))
+                {    
+                    if (data[i].currencies.EUR)
+                    {   languageData.push(data[i].name.common);   }
                 }
             }
-            console.log(languageData);
-            console.log(brojac);
+            loading.style.display = 'none';
+            // console.log("Counties talking English are:" + languageData);
+            countriesTalkingEnglishResult.innerText = `Countries using EURO:
+            
+            `
+            countriesTalkingEnglishResult.innerText += languageData;
         })
         .catch(function (err) {
             console.error('Fetch error:', err);
         });
 }
 
+// za Makedonija
 function callApiMacedonia() {
-    fetch(`https://restcountries.com/v3.1/all`)
+    fetch(`https://restcountries.com/v3.1/name/Macedonia`)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-            let languageData = [];
-            for (let i = 0; i < data.length; i++) {
-                if (data[i].languages.mkd) {
-                    languageData.push(data[i].name.common);
-                    console.log(data[i].languages);
-                    console.log(i);
-                    console.log(languageData);
-                };
-            };
-            console.log(languageData);
-            console.log(brojac);
+            let inputData = [];
+            let element = {
+                Flag: data[0].flags.png,
+                Name: data[0].name.common,
+                Population: data[0].population,
+                Capital: data[0].capital,
+                Area: data[0].area
+            }
+            console.log(element);
+            inputData.push(element);
+            loading.style.display = 'none';
+            tabela.innerHTML = "";
+            tabela.innerHTML +=
+                `<td> <img style="height: 150px; width: 250px;" src="${inputData[0].Flag}" alt="">
+                <br>
+                <b style="font-size: x-large;">${inputData[0].Name}</b>
+                <br>
+                ${inputData[0].Name} is country with population of ${inputData[0].Population}
+                with the capital city ${inputData[0].Capital}
+                <br> <br>
+                Open od <a href="https://en.wikipedia.org/wiki/${inputData[0].Name}">Wikipedia</a></td>`
+        })
+        .catch(function (err) {
+            console.error('Fetch error:', err);
         });
-};
-
-console.log("Hello World");
+}
